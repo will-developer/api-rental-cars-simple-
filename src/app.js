0b46a665b77ca3e37223b998a481c17d76013264
will-developer <william_limaa@hotmail.com>
2024-11-05 16:03:30 -0300

    chore: add knex setup, server initialization, and database config

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const consign = require('consign');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+consign({ cwd: 'src' }).include('routes').into(app);
+
+module.exports = app;
