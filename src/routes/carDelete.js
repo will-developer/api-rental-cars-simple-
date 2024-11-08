7dd43e29ad203f25f7febd200da5812f76e17223
will-developer <william_limaa@hotmail.com>
2024-11-08 19:16:44 -0300

    feat: add init structure for delete feature

---
+++ src/routes/carDelete.js
@@ -0,0 +1,16 @@
+const carDatabase = require('../database/CarDatabase')
+
+module.exports = (app) => {
+  app.delete('/api/v1/cars/:id', async (req, res) => {
+    const { id } = req.params
+
+    const car = await carDatabase.findCarById(id)
+    if (!car) {
+      return res.status(404).json({
+        errors: ['car not found']
+      })
+    }
+
+    return res.status(200).json('test ok')
+  })
+}
