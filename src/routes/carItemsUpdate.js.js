76dcf8ea0b419a463d301358a5809e68dc4cea68
will-developer <william_limaa@hotmail.com>
2024-11-11 10:22:56 -0300

    fix: syntax error in carItemsUpdate.js

--- src/routes/carItemsUpdate.js.js
+++ src/routes/carItemsUpdate.js.js
@@ -1,14 +1,21 @@
 const carItemService = require('../services/carItemService')
 const handleError = require('../errors/errorHandler')
-const validateItems = require('../services/validate-items')
 
 module.exports = (app) => {
   app.put('/api/v1/cars/:id/items', async (req, res) => {
     try {
       const { id } = req.params
       const items = req.body
+      const errors = []
 
-      const errors = validateItems(items)
+      if (items.length === 0) errors.push('Items is required')
+      if (items.length > 5) errors.push('items must be a maximum of 5')
+
+      const uniqueItems = new Set(items)
+      if (uniqueItems.size !== items.length) {
+        errors.push('items cannot be repeated')
+      }
+
       if (errors.length > 0) {
         return res.status(400).json({ errors })
       }
