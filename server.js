def9ca7d99a9aa852771721766909d452da55d8c
will-developer <william_limaa@hotmail.com>
2024-11-12 08:14:44 -0300

    chore: add jest for testing

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./src/app')
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port: ${PORT}`)
 })
